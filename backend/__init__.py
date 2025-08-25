fd2fa690063155f9a8149c917476e65b8930e535
Vaibhav Nalotia <nalotiavaibhav@gmail.com>
2025-08-25 14:53:18 +0530

    updated backend, ui

--- backend/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Backend package for Wellness at Work Eye Tracker
-"""
-from .models import User, BlinkSample, UserSession, SessionLocal, init_database
-
-__all__ = ['User', 'BlinkSample', 'UserSession', 'SessionLocal', 'init_database']
